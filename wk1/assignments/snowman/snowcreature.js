2cf057841a5165ffe64729263a5953637968c5f8
CBaniecki <cbaniecki@mcad.edu>
2021-01-24 19:41:02 -0600

    created snowcreature

--- wk1/assignments/snowman/snowcreature.js
+++ wk1/assignments/snowman/snowcreature.js
@@ -9,8 +9,19 @@ function draw(){
     background(grid);
     // snowman legs 
     fill("#ffffff");
+    strokeWeight(20);
     stroke(100, 250, 100);
-    // left 
+    // left leg
     ellipse(350, 650, 200);
+    // right leg
+    ellipse(650, 650, 200);
+    // body
+    ellipse(500, 450, 350, 400);
+    //head 
+    ellipse(500, 200, 200);
+    //hat brim
+    stroke(100, 100, 255);
+    strokeWeight(40);
+    line(400, 120, 500, 120); 
 }
 
