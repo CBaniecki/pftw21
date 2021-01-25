4fb007d4e92472607ba6e7a0d45649306445b2b5
CBaniecki <cbaniecki@mcad.edu>
2021-01-25 10:08:20 -0600

    added demo.js and .html file to demo folder

---
+++ wk2/demo/demo.js
@@ -0,0 +1,12 @@
+// from string using split
+const nameString = 'Betty|Bob|Sue';
+console.log(nameString);
+const myArrayFromString = nameString.split("|");
+console.log(myArrayFromString);
+// from square brackets (with and without content);
+const myMovies = []; 
+console.log(myMovies);
+const mySongs = ['happy birthday song', 'twinkle twinkle', 
+'popcorn popping on the apricot tree'];
+console.log(mySongs);
+const myNumbers = [1, 2, 3, 4, 5]; 
