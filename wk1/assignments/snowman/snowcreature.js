a5ff9772d9724c81d67ebaa841eecd3fa4d97166
CBaniecki <cbaniecki@mcad.edu>
2021-01-24 22:34:15 -0600

    editing snowman features in .js file

--- wk1/assignments/snowman/snowcreature.js
+++ wk1/assignments/snowman/snowcreature.js
@@ -1,27 +1,54 @@
 let grid = undefined; 
+let stroke1 = prompt("enter a basic color name in lower case", "gray");
+let stroke2 = prompt("enter another basic color lowercase", "purple");
+let fill1 = prompt("enter a basic color in lowercase", "black");
+
 function setup(){
     createCanvas(1000, 800,);
-    background("#ccc");
+    background("#1ae");
     grid = loadImage("../images/100px_grid.png");
 } 
 
 function draw(){
-    background(grid);
+    //background(grid);
     // snowman legs 
     fill("#ffffff");
     strokeWeight(20);
-    stroke(100, 250, 100);
-    // left leg
-    ellipse(350, 650, 200);
-    // right leg
-    ellipse(650, 650, 200);
+    stroke(stroke1);
+    // bottom
+    ellipse(510, 650, 450);
     // body
-    ellipse(500, 450, 350, 400);
+    ellipse(510, 430, 350, 350);
     //head 
     ellipse(500, 200, 200);
     //hat brim
-    stroke(100, 100, 255);
-    strokeWeight(40);
-    line(400, 120, 500, 120); 
+    stroke(stroke2);
+    strokeWeight(30);
+    fill(fill1); 
+    line(290, 120, 700, 120); 
+    //hat body 
+    quad(400, 50, 610, 50, 610, 120, 400, 120);
+    // eyes 
+    stroke(0);
+    strokeWeight(50);
+    point(425, 200); 
+    point(575, 200);
+    //mouth 
+    noFill();
+    strokeWeight(10);
+    arc(500, 240, 100, 90, 0, HALF_PI);
+    //left arm
+    noFill();
+    strokeWeight(10);
+    arc(250, 330, 200, 343, 200, QUARTER_PI);
+    arc(220, 210,200, 100, 200,QUARTER_PI);
+    //right arm
+    noFill();
+    strokeWeight(10);
+    arc(600, 330, 200, 300, 200, QUARTER_PI);
+    arc(670, 260, 90, 100, 430, QUARTER_PI);
+    
+    
+
 }
 
