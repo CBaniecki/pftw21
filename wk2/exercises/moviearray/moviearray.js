5f6ac37e332db230e79a9c0b5155ca278a95e19d
CBaniecki <cbaniecki@mcad.edu>
2021-01-27 13:51:48 -0600

    added movie array folder and .html and .js files to exercises folder

---
+++ wk2/exercises/moviearray/moviearray.js
@@ -0,0 +1,8 @@
+let myArray = ['ghostbusters', 'forrest gump', 'planes, trains and automobiles', 'once upon a crime', 'up', 'sing'];
+console.log(myArray);
+
+const movie = window.prompt('what is your favorite movie?');
+
+myArray.push(movie);
+
+console.log(myArray.toString());
