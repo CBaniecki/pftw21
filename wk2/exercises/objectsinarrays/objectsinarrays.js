7f207526a2b51b0c6164f88c6a40dcadfc9b9ea5
CBaniecki <cbaniecki@mcad.edu>
2021-01-28 10:44:21 -0600

    updated array objects in .js file

--- wk2/exercises/objectsinarrays/objectsinarrays.js
+++ wk2/exercises/objectsinarrays/objectsinarrays.js
@@ -1,7 +1,7 @@
 const games = [
-    {title: 'Checkers', type: 'Strategy', numberOfPlayers: '2', shortDescription:'Checkers is a strategy board game for two players which involve diagonal moves of game pieces and captures by jumping over opponent pieces.'},
-    {title: 'Monopoly', type: 'Board', numberOfPlayers: '2 to number of pawns in the box', shortDescription:'Monopoly is a fast-dealing property trading game where players roll two six-sided dice to move around the game board, buying and trading properties and using the properties to purchase houses and hotels.'},
-    {title: 'Sorry!', type: 'Board', numberOfPlayers: '2 to 4', shortDescription:'Sorry is a fun board game where players move their four pawns around the board, attempting to get all of their pieces "home" before any other player.'}
+    {title: 'Checkers', type: 'Strategy', numberOfPlayers: '2', shortDescription:'Checkers is a strategy board game for two players which involve diagonal moves of game pieces and captures by jumping over opponent pieces'},
+    {title: 'Monopoly', type: 'Board', numberOfPlayers: '2 players and can reach up to however many pawns are in the box.', shortDescription:'Monopoly is a fast-dealing property trading game where players roll two six-sided dice to move around the game board, buying and trading properties'},
+    {title: 'Sorry!', type: 'Board', numberOfPlayers: '2 to 4', shortDescription:'Sorry is a fun board game where players move their four pawns around the board, attempting to get all of their pieces "home" before any other player'}
 ]; 
 
 const gameArray = [
@@ -16,7 +16,7 @@ if(firstQuestion == gameArray[0].answer){
     alert('You selected ' + games[0].title + ' which is a ' + games[0].type + ' game with ' + games[0].numberOfPlayers + ' players. A description of this game is: ' + games[0].shortDescription + ".");
 
 } if(firstQuestion == gameArray[1].answer){ 
-    alert('You selected ' + games[1].title + ' which is a ' + games[1].type + ' game with ' + games[1].numberOfPlayers + ' players. A description of this game is: ' + games[1].shortDescription + ".");
+    alert('You selected ' + games[1].title + ' which is a ' + games[1].type + ' game with ' + games[1].numberOfPlayers + ' A description of this game is: ' + games[1].shortDescription + ".");
 } if(firstQuestion == gameArray[2].answer){
     alert('You selected ' + games[2].title + ' which is a ' + games[2].type + ' game with ' + games[2].numberOfPlayers + ' players. A description of this game is: ' + games[2].shortDescription + ".");
 }
