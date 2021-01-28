f68137a526ef1884f2f1d68418e76f58fd4710cb
CBaniecki <cbaniecki@mcad.edu>
2021-01-28 10:27:25 -0600

    updated .js file by adding question and answer prompts

--- wk2/exercises/objectsinarrays/objectsinarrays.js
+++ wk2/exercises/objectsinarrays/objectsinarrays.js
@@ -6,13 +6,17 @@ const games = [
 
 const gameArray = [
     {question: 'I have three games in my collection. Pick a number between 1 and 3 and I will tell you about that game', answer: '1'},
+    {answer: '2'},
+    {answer: '3'},
+    
 ];
 
 const firstQuestion = prompt(gameArray[0].question);
 if(firstQuestion == gameArray[0].answer){
-    alert('You selected ' + games[0].title + ' which is a ' + games[0].type + ' game with ' + games[0].numberOfPlayers + ' players. A description of this game is ' + games[0].shortDescription + ".");
-} else{
-    alert('You selected ' + games[1].title + ' which is a ' + games[1].type + ' game with ' + games[1].numberOfPlayers + ' players. A description of this game is ' + games[1].shortDescription + ".");
-} else{
-    alert('You selected ' + games[2].title + ' which is a ' + games[2].type + ' game with ' + games[2].numberOfPlayers + ' players. A description of this game is ' + games[2].shortDescription + ".");
-} 
+    alert('You selected ' + games[0].title + ' which is a ' + games[0].type + ' game with ' + games[0].numberOfPlayers + ' players. A description of this game is: ' + games[0].shortDescription + ".");
+
+} if(firstQuestion == gameArray[1].answer){ 
+    alert('You selected ' + games[1].title + ' which is a ' + games[1].type + ' game with ' + games[1].numberOfPlayers + ' players. A description of this game is: ' + games[1].shortDescription + ".");
+} if(firstQuestion == gameArray[2].answer){
+    alert('You selected ' + games[2].title + ' which is a ' + games[2].type + ' game with ' + games[2].numberOfPlayers + ' players. A description of this game is: ' + games[2].shortDescription + ".");
+}
