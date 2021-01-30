85ebab348844c07098b4e715af5524bc59413601
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-01-30 15:17:31 -0600

    updated array of questions.js

--- wk2/assignment/arrayofquestions/arrayofquestions.js
+++ wk2/assignment/arrayofquestions/arrayofquestions.js
@@ -1,5 +1,7 @@
 const  triviaQuestion = [
     {question: 'what is the name of the famous clock tower in London?', answer: 'Big Ben'},
+    {question: 'what city in France is the Eiffel Tower located in?', answer:'Paris'},
+    {question: 'where is Yosemite National Park located?', answer :'California'}
 ];
 
 const firstQuestion = prompt(triviaQuestion[0].question); {
@@ -7,7 +9,6 @@ alert('You answered ' + firstQuestion + ' the correct answer was ' + triviaQuest
 }
 
 const arrayOfQuestions = triviaQuestion.concat(firstQuestion);
-const newArrayOfQuestions = arrayOfQuestions.concat();
-console.log(newArrayOfQuestions); 
+console.log(arrayOfQuestions); 
 
 
