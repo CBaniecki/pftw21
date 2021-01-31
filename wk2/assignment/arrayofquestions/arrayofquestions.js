468c395acd686dea63c1bacf8f121e55914a681f
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-01-30 18:49:06 -0600

    added questions to array in .js file

--- wk2/assignment/arrayofquestions/arrayofquestions.js
+++ wk2/assignment/arrayofquestions/arrayofquestions.js
@@ -1,13 +1,17 @@
 const  triviaQuestion = [
-    {question: 'what is the name of the famous clock tower in London?', answer: 'Big Ben'},
-    {question: 'what city in France is the Eiffel Tower located in?', answer:'Paris'},
-    {question: 'where is Yosemite National Park located?', answer :'California'}
+    {question: 'What is the name of the famous clock tower in London?', answer: 'Big Ben'},
+    {question: 'What city in France is the Eiffel Tower located in?', answer:'Paris'},
+    {question: 'Where is Yosemite National Park located?', answer :'California'},
+    {question: 'What was the first Pixar movie?', answer: 'Toy Story'},
+    {question: 'What is the bunny rabbita name in Bambi?', answer: 'Thumper'}
 ];
 
 const randomIdx = Math.round(Math.random() * triviaQuestion.length -1);
 
 firstQuestion = prompt(triviaQuestion[randomIdx].question); {
-alert('You answered ' + firstQuestion + ' the correct answer was ' + triviaQuestion[randomIdx].answer);
+alert('You answered ' + firstQuestion + "." + "\n" +
+
+'The correct answer was ' + triviaQuestion[randomIdx].answer);
 }
 
 const arrayOfQuestions = triviaQuestion.concat(firstQuestion);
