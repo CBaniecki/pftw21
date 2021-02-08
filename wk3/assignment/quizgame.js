ee9f3f86946cf1eb882dc9702e00c4ea42253650
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-02-07 21:23:54 -0600

    Created and edited quizgame.js

---
+++ wk3/assignment/quizgame.js
@@ -0,0 +1,16 @@
+let questionInput;
+let currentQuestion;
+let response;
+let responseColor='green';
+let heading;
+let numberCorrect = Number(0);
+let numberWrong = Number(0);
+let quizQuestions = [
+       {question: 'What is the name of the Disney movie where two dogs share spaghetti?', answer: 'Lady and the Tramp'},
+       {question: 'Faith, trust and ?', answer:'Pixie dust'},
+       {question: 'There are three mountains in Magic Kindom, What are their names? ', answer :'Space Mountain, Splash Mountain, Big Thunder Railroad Mountain'},
+       {question: 'What was the first Pixar movie?', answer: 'Toy Story'},
+       {question: 'What is the bunny rabbits name in Bambi?', answer: 'Thumper'},
+       {question: 'What is the name of the animation movie created by Walt Disney Animation Studios in 1988?', answer: 'Who Framed Roger Rabbit?'}
+
+]
