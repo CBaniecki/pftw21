e45c461eb02259a27cc7661c2567c8112d09a952
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-02-07 22:51:20 -0600

    updated quiz.js file

--- wk3/assignment/quizgame.js
+++ wk3/assignment/quizgame.js
@@ -1,17 +1,17 @@
 let questionInput;
 let currentQuestion;
 let response;
-let responseColor='green';
+let responseColor='white';
 let heading;
 let numberCorrect = Number(0);
 let numberWrong = Number(0);
 let statements = [
-    {question: 'What is the name of the famous clock tower in London?', answer: 'Big Ben'},
-    {question: 'What city in France is the Eiffel Tower located in?', answer:'Paris'},
-    {question: 'Where is Yosemite National Park located?', answer :'California'},
+    {question: 'What is the name of the Disney movie where \n two dogs share spaghetti?', answer: 'Lady and the Tramp'},
+    {question: 'Faith, Trust and ?', answer:'Pixie Dust'},
+    {question: 'There are three mountains in Magic Kingdom, \n What are their names?', answer :'Space Mountaon, Splash Mountain and Big Thunder Railroad Mountaon'},
     {question: 'What was the first Pixar movie?', answer: 'Toy Story'},
     {question: 'What is the bunny rabbits name in Bambi?', answer: 'Thumper'},
-    {question: 'What is the name of the animation movie created by Walt Disney Animation Studios in 1988?', answer: 'Who Framed Roger Rabbit?'}
+    {question: 'What is the name of the animation movie created \n by Walt Disney Animation Studios in 1988?', answer: 'Who Framed Roger Rabbit?'}
 
 ]; 
 
@@ -44,7 +44,7 @@ function checkQuestion(){
      });
      // this is the correct condition
      response = 'correct! next question';
-     responseColor= 'green';
+     responseColor= 'white';
  } else {
      // this is the wrong answer 
      response = 'oops, that wasn\'t quite right! Try again';
@@ -66,19 +66,28 @@ function setup() {
  heading = createElement('h1', ['Disney Trivia Quiz']);
  heading.position(100, 100);
  questionInput = createInput('');
- questionInput.size(250, 26);
- questionInput.position(100, 240);
+ questionInput.size(300, 40);
+ questionInput.position(100, 300);
  submitAnswerButton = createButton('submit answer');
  submitAnswerButton.size(250, 40);
  submitAnswerButton.mousePressed(checkQuestion);
- submitAnswerButton.position( 100, 300);
+ submitAnswerButton.position( 100, 400);
 }
 
 function draw() {
- background('lightblue');
- fill('black');
+ //background('lightblue');
+ fill('white');
  textSize(24);
  text(message, 100, 200);
  fill(responseColor);
  text(response, 100, 350);
 }
+
+function backgroundImg(imgNumber) {
+var myImages = ["images/DisneyBackground.jpg", "images/disneycastle.jpeg"];
+var image= document.body.style.backgroundImage;
+var newImg = Math.floor(Math.random()* myImages.length);
+document.body.style.backgroundImage = 'url('+myImages[newImg]+')';
+
+}
+window.onload = backgroundImg;
