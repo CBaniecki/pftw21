df6bed0ff128d2bff6a20447bb279416ad85add6
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-02-10 11:12:39 -0600

    created and edited DOM exercise .js file

---
+++ wk4/DOMexercise/DOMexercise.js
@@ -0,0 +1,14 @@
+// select empty div and assign to a variable
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
+
+// put an h1 inside of it 
+// create a new element
+const heading = document.createElement('h1');
+heading.innerHTML = 'Llamas and Camels come from the same family, which makes them distant cousins';
+heading.style.cursor = 'pointer'; 
+heading.addEventListener('click', handleHeadingClick);
+myEmptyDiv.appendChild(heading);
+function handleHeadingClick () {
+    console.log('Heading has been clicked');
+    document.body.style.backgroundColor = 'lavender';
+}
