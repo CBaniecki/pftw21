ca144ff55ccda92c3c52eef49d5d431004edc360
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-02-14 19:32:23 -0600

    updated memorygame.js

--- wk4/assignment/memorygame.js
+++ wk4/assignment/memorygame.js
@@ -1,22 +1,22 @@
-const circleDiameter = 100;
-let startingX = 150;
-let startingY = 100;
+const rectangleDiameter = 100;
+let startingX = 530;
+let startingY = 80;
 let myRects = [];
 let startingId = 0;
 function setup () {
-    createCanvas(1000, 500);
+    createCanvas(1000, 720);
     background('#fceeda');
     //ellipse(circleX, circleY, circleDiameter);
-    for (let k = 0; k < 2; k ++){
-        for (let i = 0; i < 5; i++) {
-            rect(startingX, startingY, circleDiameter);
+    for (let k = 0; k < 4; k ++){
+        for (let i = 0; i < 2; i++) {
+            rect(startingX, startingY, rectangleDiameter);
             myRects.push({ x: startingX, y: startingY, id: startingId});
             startingX += 150;
             startingId++;
         }   
 
         startingY += 140; 
-        startingX = 150;
+        startingX = 530;
 
     }
   
