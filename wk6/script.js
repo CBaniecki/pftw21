cc9f1f9958e519a067c4ab9f674571b1b2e9e434
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-02-28 15:30:05 -0600

    created Vue assignment index.html and .js files

---
+++ wk6/script.js
@@ -0,0 +1,21 @@
+
+
+ var llamaList = new Vue ({
+    el:'#llamaList',
+   
+    data: {
+        llamas: [
+            {message: 'Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands'},
+            {message: 'Llamas are smart and easy to train.'},
+            {message: 'Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white'},
+            {message: 'Llamas are especially vocal. Mothers often hum to communicate with their babies, called crias.'},
+            {message: 'Llamas are sometimes used for protection. Farmers use llamas to guard herds of small animals.'},
+            {message: 'Llamas can be trained as professional therapy animals.'},
+            {message: 'Llamas are the hippie cousins to the camel species'},
+            {message: 'Llamas have a gentle and curious personality and are independent.'},
+            {message: 'Llamas live in the Andes mountains of South America'}
+        ]
+
+    }
+})
+
