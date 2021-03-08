9d5a5007d7f4b62ad5a3f9e0698527a5031d64f7
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-03-07 22:07:42 -0600

    created .js file in assignments

---
+++ wk7/assignment/script.js
@@ -0,0 +1,13 @@
+var demo = new Vue ({
+    el:"#demo",
+
+    data: {
+        cameras: [
+            {name: "Lomography Belair Trailblaizer", type: "Medium Format", year:"2012", film: "Yes"},
+            {name: "Nikon D3100", type:"Digitial SLR", year: "2010", film: "No"},
+            {name: "Nikon N75", type: "35mm SLR", year:"2003", film: "Yes"},
+            {name: "Polaroid Land Camera", type: "Self-developing film", year: "1948", film: "Yes"},
+            {name: "Rolleiflex Medium Format TLR", type: "Twin Lens Reflex", year:"1920",film:"Yes"},
+        ]
+    }
+});
