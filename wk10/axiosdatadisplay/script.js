cd267960c9b76caf9f7bd9d6dd718dc2f3299997
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-04-05 18:10:35 -0500

    updated script.js in axiosanddatadisplay file

--- wk10/axiosdatadisplay/script.js
+++ wk10/axiosdatadisplay/script.js
@@ -1,20 +1,19 @@
 Vue.component('artwork-departments', {
-    template: `<div> {{displayName}} </div>`,
-    props: ['displayName']
-})
-
+    template: `<div>{{department.displayName}} </div>`,
+    props: ['department']
+});
 
 const vm = new Vue ({
     el: "#display",
     data:{ 
-        results: []
+        departments: []
     },
 
 
-    mounted () {
-        axios.get('https://collectionapi.metmuseum.org/public/collection/v1/departments').then(response => {
-            console.log('response', response);
-            vm.results = response.data;
+    mounted() {
+        axios.get("https://collectionapi.metmuseum.org/public/collection/v1/departments").then(function (response) {
+            console.log('response', response.data.departments);
+            vm.departments = response.data.departments;
         })  
     }
 });
