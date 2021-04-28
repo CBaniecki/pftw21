616dee1195ad6cf6dc416a7572754c7287722a7c
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-04-28 14:18:39 -0500

    edited timer, added flowers

--- wk13/assignment/p5.js
+++ wk13/assignment/p5.js
@@ -1,30 +1,30 @@
+
+//flower code is found from p5.js website
+
 //water drop array
 var drops= [];
 
 //timer to stop rain
-var time1 = 5200;
+var time1 = 5220;
 var time2 = 5310;
 
+
+
 //flower
-let diameter;
-let angle = 0;
-var pos;
+var rando = 0.00;
+var co = 100;
+
+
 
 function setup() {
     createCanvas(740,640);
+    //creates water droplets
     for(var i = 0; i < 500; i++) {
         drops[i] = new Drop();
     }
-
-    diameter = height - 10;
-    stroke(0);
-    strokeWeight(2);
-    fill(0);
-
-
 }
 function draw() {
-    background("#000080");
+    background("#add8e6"); 
     var currentTime = millis();
 
     //timer which sets duration for water
@@ -37,13 +37,31 @@ function draw() {
 
     //flower
     if(currentTime > time2){
-    let d1 = 10 + (sin(angle) * diameter) /6 + diameter/5;
-    let d2 = 10 + (sin(angle) * diameter) /7 + diameter/6;
-    
-    ellipse(150, height/2, d1, d1);
-    ellipse(width/5, height/3.5, d2, d2);
 
-    angle += 0.02;
+      co = co + 1;
+      var zo = floor(noise(co) * 20);
+
+      rando = rando + 0.0005
+      var no = noise(rando);
+
+      //flower 1
+      translate(height/2, width/2);
+      fill(mouseX, zo, 150, 127);
+      noStroke();
+      for( i = 0; i < 10; i++ ) {
+          ellipse(0, 40, 30, 80);
+          rotate(QUARTER_PI/no);
+      }
+
+      //flower 2
+      translate(60, 200);
+      fill("#9932cc");
+      for( i = 0; i < 11; i++ ) {
+          ellipse(0, 20, 40, 100);
+          rotate(TWO_PI/no);
+      }
+        
+    
     }
 
 }
@@ -69,7 +87,7 @@ function Drop() {
     this.show = function() {
         var thick = map(this.z, 0, 20, 1, 3);
         strokeWeight(thick);
-        stroke(255);
+        stroke("#000080");
         line(this.x, this.y, this.x, this.y + this.len);
     }
 }
