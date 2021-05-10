94980ece6af459570c20d7304b9bfccb676ba796
Clarissa Baniecki <cbaniecki@mcad.edu>
2021-05-10 11:45:03 -0500

    reconfigured image array and card back image

--- wk14/script.js
+++ wk14/script.js
@@ -3,7 +3,6 @@ const UP = 'up';
 let startingX = 100;
 let startingY = 100;
 let cards = [];
-let rSlider, gSlider, bSlider;
 const cardGame = {
    totalPairs: 5,
    flippedCards: [],
@@ -14,7 +13,7 @@ const cardGame = {
 let cardfaceArray = [];
 let cardback;
 function preload() {
-    cardback = loadImage('images/yellowbouquet-copy.jpg');
+    cardback = loadImage('images/card-back-ribbon.jpg');
     cardfaceArray = [
         loadImage('images/cardback-quote-3.jpg'),
         loadImage('images/cardback-quote-1-1.jpg'),
@@ -22,7 +21,6 @@ function preload() {
         loadImage('images/cardback-quote-6-1.jpg'),
         loadImage('images/cardback-quote-9.jpg'),
     ]
-
 }
 
 function setup() {
@@ -35,10 +33,10 @@ function setup() {
         selectedFaces.push(face);
         //remove the used cardface so it doesn't get randomly selected again
         cardfaceArray.splice(randomIdx, 1);
-
     }
     selectedFaces = shuffleArray(selectedFaces);
-
+    //layout for cards
+    // j is for the rows and i is for the number of cards per row
     for (let j = 0; j < 2; j++){
         for (let i = 0; i < 5; i++){
             const faceImage = selectedFaces.pop();
@@ -47,39 +45,37 @@ function setup() {
         }
         startingY += 280;
         startingX = 100;
-    }
-    
-
-
+    } 
 }
 
 function draw () {
- 
-    
-
     background(0);
-
     if(cardGame.numMatched === cardGame.totalPairs) {
         fill(255);
         textSize(56);
-        text('You Win!', 600, 695);
+        text('You Won!', 600, 695);
         noLoop();
     }
     for(let k = 0; k < cards.length; k++) {
+        //if cards don't match, this turns the cards down 
         if(!cards[k].isMatch) {
             cards[k].face = DOWN;
         }
         cards[k].show();
-
     }
+
     noLoop();
     cardGame.flippedCards.length = 0;
     cardGame.waiting = false;
     fill(255);
     textSize(36);
-    text('Matches: ' + cardGame.numMatched, 100, 690);
+    text('Matches: ' + cardGame.numMatched, 100, 700);
     text('Attempts: ' + cardGame.attempts, 100, 760);
 
+    textSize(30);
+    text('Accessibility Card Game', 600, 60);
+    textSize(20);
+    text('White ribbon represents disabilities', 100, 812);
 }
 
 function mousePressed(){
@@ -95,8 +91,8 @@ function mousePressed(){
     if (cardGame.flippedCards.length === 2){
         cardGame.attempts++;
        if (cardGame.flippedCards[0].cardFaceImg === cardGame.flippedCards[1].cardFaceImg){
-           //cards match! Time to score!
-           // mark cards as matched so they don't flip bacl
+           //Cards match! 
+           // mark cards as matched to prevent them from flipping back
            cardGame.flippedCards[0].isMatch = true;
            cardGame.flippedCards[1].isMatch = true;
            //empty flipped cards array
@@ -125,8 +121,8 @@ class Card {
         this.cardFaceImg = cardFaceImg;
         this.isMatch = false;
         this.show();
-
     }
+
     show () {
         if(this.face === UP || this.isMatch){
             fill('#aaa');
@@ -134,45 +130,43 @@ class Card {
             image(this.cardFaceImg, this.x + 5 , this.y + 10);
 
         } else {
-            fill('lavender');
+            fill(247);
             rect(this.x, this.y, this.width, this.height);
-            image(cardback, this.x + 5, this.y + 10);
-           
+            image(cardback, this.x + 35, this.y + 20); 
         }
-        
     }
 
     didHit (mouseX, mouseY) {
-        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height){
+        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
             this.flip()
             return true;
         } else {
             return false;
         }
     }
+    //controls the interactivity of the flip
     flip () {
         if(this.face === DOWN) {
             this.face = UP;
+
         } else {
-            this.face = DOWN;
+            this.face = DOWN
         }
         this.show();
     }
-
 }
-
+//shuffles the cards
 function shuffleArray (array) {
     let counter = array.length;
     while(counter > 0) {
-        //pick random index
+        //choose random index
         const idx = Math.floor(Math.random() * counter);
-        //decrease counter by 1 (decrement)
+        //decrease counter by 1 
         counter --;
         //swap the last element with it
         const temp = array[counter];
         array[counter] = array[idx];
         array[idx] = temp;
-
     }
     return array;
 }
